5b3da4cf72c34c6e4eee2b69ff8e014a1d758ff2
Stilglar <65299070+Stilglar@users.noreply.github.com>
2020-05-18 17:03:05 +0200

    Añadido unas lineas nuevas

--- creaciondetablasydatos.sql
+++ creaciondetablasydatos.sql
@@ -1,6 +1,6 @@
 /*
  * Autor: Jon de ABC.com
- * Objetivo: Creación de tablas y datos para usar como ejemplos
+ * Objetivo: CreaciÃ³n de tablas y datos para usar como ejemplos
  * Fecha: 18 de mayo, 2010
  */
 
@@ -11,4 +11,16 @@ CREATE TABLE TiposdeProductos (
 );
 
 INSERT INTO TiposdeProductos (TipoID, Nombre, Descripcion)
-VALUES(1, 'Bebidas', 'Refrescos, cafés, tes, cervezas');
+VALUES(1, 'Bebidas', 'Refrescos, cafÃ©s, tes, cervezas');
+
+
+INSERT INTO TiposdeProductos (TipoID, Nombre, Descripcion)
+VALUES(2, 'Carnes', 'vaca, cerdo, buey, cordero');
+
+
+INSERT INTO TiposdeProductos (TipoID, Nombre, Descripcion)
+VALUES(3, 'Pescados', 'lubina, rape, salmon, ');
+
+
+INSERT INTO TiposdeProductos (TipoID, Nombre, Descripcion)
+VALUES(4, 'Postres', 'helado, tarta, mousse, fruta');
